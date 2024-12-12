1fbcf30f20b95071fe97127837abad895e79f405
yeasar <syeasar.kuet@gmail.com>
2024-12-12 14:43:18 +0600

    :ghost: chores: remove unnecessary comments and code

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,4 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   redirect("/home");
-
-  // The following JSX will not be rendered due to the redirect
-  return <div>{/* This content will not be shown due to redirection */}</div>;
 }
